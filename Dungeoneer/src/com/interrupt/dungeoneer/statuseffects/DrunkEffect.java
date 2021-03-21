cdc98b3d93d0e0c90f9c3a8030a13e3b445f238f
HelloImCDD <cddgamer@gmail.com>
2021-03-21 21:25:04 +0100

    DrunkEffect: moved hardcoded data to a variable. Created Getters/Setters and a Constructor

--- Dungeoneer/src/com/interrupt/dungeoneer/statuseffects/DrunkEffect.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/statuseffects/DrunkEffect.java
@@ -5,6 +5,8 @@ import com.interrupt.managers.StringManager;
 
 public class DrunkEffect extends StatusEffect {
 
+    private float drunkFactor = 0.025f;
+
     public DrunkEffect() {
         this(2700);
     }
@@ -15,8 +17,21 @@ public class DrunkEffect extends StatusEffect {
         this.statusEffectType = StatusEffectType.DRUNK;
     }
 
+    public DrunkEffect(int time, int drunkFactor) {
+        this(time);
+        this.drunkFactor = drunkFactor;
+    }
+
     @Override
     public void doTick(Actor owner, float delta) {
-        owner.drunkMod += delta * 0.025f;
+        owner.drunkMod += delta * drunkFactor;
+    }
+
+    public float getDrunkFactor() {
+        return drunkFactor;
+    }
+
+    public void setDrunkFactor(float drunkFactor) {
+        this.drunkFactor = drunkFactor;
     }
 }
